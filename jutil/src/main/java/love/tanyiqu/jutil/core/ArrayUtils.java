ad37abd7e2d2006c219af053cac206552c33b6e6
Tanyiqu <1953649096@qq.com>
2021-04-10 22:59:23 +0800

    Add array utils: indexOf

--- jutil/src/main/java/love/tanyiqu/jutil/core/ArrayUtils.java
+++ jutil/src/main/java/love/tanyiqu/jutil/core/ArrayUtils.java
@@ -11,6 +11,9 @@ import java.util.Arrays;
 @SuppressWarnings({"unused", "DuplicatedCode"})
 public final class ArrayUtils {
 
+
+    static final int INDEX_NOT_FOUND = -1;
+
     ///////////////////////////////////////////////////////////////////////////
     // new array
     ///////////////////////////////////////////////////////////////////////////
@@ -415,4 +418,35 @@ public final class ArrayUtils {
         return subArray;
     }
 
+
+    ///////////////////////////////////////////////////////////////////////////
+    // indexOf
+    ///////////////////////////////////////////////////////////////////////////
+
+    public static int indexOf(@Nullable Object[] array, @Nullable Object objectToFind) {
+        return indexOf(array, objectToFind, 0);
+    }
+
+    public static int indexOf(@Nullable Object[] array, @Nullable final Object objectToFind, int startIndex) {
+        if (array == null) {
+            return INDEX_NOT_FOUND;
+        }
+        if (startIndex < 0) {
+            startIndex = 0;
+        }
+        if (objectToFind == null) {
+            for (int i = startIndex; i < array.length; i++) {
+                if (array[i] == null) {
+                    return i;
+                }
+            }
+        } else {
+            for (int i = startIndex; i < array.length; i++) {
+                if (objectToFind.equals(array[i])) {
+                    return i;
+                }
+            }
+        }
+        return INDEX_NOT_FOUND;
+    }
 }
