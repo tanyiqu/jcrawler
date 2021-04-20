85fdf776ca0854fd7d261ab1ce3ac37e3829d072
Tanyiqu <1953649096@qq.com>
2021-04-20 21:33:40 +0800

    Add convert utils: int to hexString

---
+++ jutil/src/test/java/love/tanyiqu/testutil/ConvertUtilsTest.java
@@ -0,0 +1,14 @@
+package love.tanyiqu.testutil;
+
+import love.tanyiqu.jutil.core.ConvertUtils;
+import org.junit.Test;
+
+public class ConvertUtilsTest {
+
+    @Test
+    public void test() {
+
+        System.out.println(ConvertUtils.int2HexString(1024));
+    }
+
+}
